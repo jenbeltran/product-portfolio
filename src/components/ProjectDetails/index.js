cffc2b842cf0533b5586b8e1540ff4ce46c67b32
jenbeltran <francescajenbeltran@gmail.com>
2020-04-02 19:14:02 -0400

    project details page complete

--- src/components/ProjectDetails/index.js
+++ src/components/ProjectDetails/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CardSection, Wrapper } from './css';
+import { CardSection, Wrapper, CardImg, Headline, Subtitle } from './css';
 import Project from '../Projects/data';
 import { ProjectSubtitles } from '../Projects/data';
 
@@ -7,19 +7,20 @@ const CardImage = (props) => {
 	const matchPageName = (Project) => Project.pageName === props.pageName;
 	const projectData = Project.find(matchPageName);
 
-	const { pageName, photo, headline, goal, action, measureOfSuccess } = projectData;
+	const { pageName, photo, alt, headline, goal, action, measureOfSuccess } = projectData;
 
 	const { subtitleOne, subtitleTwo, subtitleThree } = ProjectSubtitles;
 
 	return (
 		<CardSection pageName={pageName} id={pageName} photo={photo}>
+			<CardImg src={photo} alt={alt} />
 			<Wrapper>
-				<h2>{headline}</h2>
-				<p>{subtitleOne}</p>
+				<Headline>{headline}</Headline>
+				<Subtitle>{subtitleOne}</Subtitle>
 				<p>{goal}</p>
-				<p>{subtitleTwo}</p>
+				<Subtitle>{subtitleTwo}</Subtitle>
 				<p>{action}</p>
-				<p>{subtitleThree}</p>
+				<Subtitle>{subtitleThree}</Subtitle>
 				<p>{measureOfSuccess}</p>
 			</Wrapper>
 		</CardSection>
